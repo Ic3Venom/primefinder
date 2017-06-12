d48d9d4385837be3487ebe1b5bd74753316fef13
Ic3Venom <Ic3Venom@gmail.com>
2017-06-12 07:31:58 -0700

    Add files via upload

---
+++ primes3.py
@@ -0,0 +1,28 @@
+#This program was a result of my first idea of finding primes 
+import time
+
+def main(): 
+    maxRange = 100000#int(raw_input("What is the max range of numbers you want tested for being prime? ")) 
+    primes = [2]
+    
+    if maxRange % 2 == 0: #To avoid even number decrements
+        maxRange -= 1
+
+    for i in xrange(3, maxRange +1, 2):
+        for j in primes:
+            if i % j == 0:
+                break
+        else:
+            primes.append(int(i))
+        
+    print 'Total primes: %d' % len(primes)
+
+if __name__ == '__main__':
+    start_time = time.time()
+    
+    main()
+    
+    print("Time taken: %s seconds" % (time.time() - start_time))
+    raw_input()
+
+                
