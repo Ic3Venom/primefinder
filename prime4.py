df57733564c3df4d012c4ec58794b27e99c90d8e
Ic3Venom <Ic3Venom@gmail.com>
2017-06-15 22:25:13 -0700

    Update prime4.py

--- prime4.py
+++ prime4.py
@@ -2,17 +2,21 @@
 import time
 
 def main(): 
-    maxRange = int(raw_input("What is the max range of numbers you want tested for being prime? "))
-
+    maxRange = 1000000#int(raw_input("What is the max range of numbers you want tested for being prime? "))
+    
     if maxRange % 2 == 0: #To avoid even number decrements
         maxRange -= 1
     
     with open('primelist.bin', 'wb+') as file:
-        file.write('2 ')
+        file.write('2')
     
-        for guess in xrange(3, maxRange +1, 2):
-            #something
-            pass
+        for guess in xrange(3, maxRange +1, 2):            
+            file.seek(0)
+            for lines in file:
+                if guess % int(lines) is 0:
+                    break
+            else:
+                file.write('\n' + bytes(guess))
             
         file.seek(0)
         num = 0
