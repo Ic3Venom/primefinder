d2b938f35d5ea937b5be5fc6a35602266fe48af4
Ic3Venom <Ic3Venom@gmail.com>
2017-06-17 22:11:00 -0700

    Update prime2.py

--- prime2.py
+++ prime2.py
@@ -7,29 +7,31 @@ def main():
     if maxRange % 2 == 0: #To avoid even number decrements
         maxRange -= 1
 
-    with open('primelist.bin', 'wb+') as file:
+    with open('primelist2.bin', 'wb+') as file:
         file.write('2 ')
         
         for i in xrange(3, maxRange +1, 2):
             for j in xrange(3, i, 2):
                 if i % j == 0:
                     break
+                
             else:
                 file.write(str(i) + ' ')
         
         file.seek(0)
-        num = 0
+        num = -1
+        
         for line in file:
-            for num, nul in enumerate(line.split(' ')):
-                pass
+            for i in line.split(' '):
+                num += 1
+                
         print 'Total primes: %d' % num
 
 if __name__ == '__main__':
     start_time = time.time()
+    print 'Prime2'
     
     main()
     
     print("Time taken: %s seconds" % (time.time() - start_time))
     raw_input()
-
-                
