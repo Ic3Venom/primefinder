42afac66f0cc0154e9e997c3e76ee6acf68c302a
Julian Meyn <Ic3Venom@gmail.com>
2017-09-27 21:42:27 -0700

    Update main.c

--- main.c
+++ main.c
@@ -3,12 +3,16 @@
 
 int main()
 {
-    long i, max, buffer;
+    unsigned long long i, max, buffer;
     short br = 0;
     FILE *fPrime = fopen("primelistc.bin", "wb+");
 
     printf("Finding the primes between 2 and what number? ");
-    scanf("%li", &max);
+    scanf("%llu", &max);
+
+    buffer = 2;
+    fwrite(&buffer, sizeof(buffer), 1, fPrime);
+    exit(0);
 
     for(i = 3; i <= max; i+= 2)
     {
@@ -16,8 +20,8 @@ int main()
 
         while( 1 )
         {
-            fread(&buffer, sizeof(long), 1, fPrime);
-
+            fread((void*)(&buffer), sizeof(buffer), 1, fPrime);
+            printf("%llu", buffer);
             if(feof(fPrime))
             {
                 break;
@@ -30,9 +34,11 @@ int main()
         }
         if(!br)
         {
-            printf("\n%li", buffer);
+            printf("\n%llu", buffer);
             fwrite(&buffer, sizeof(long), 1, fPrime);
         }
+
+        br = 0;
 
     }
 
