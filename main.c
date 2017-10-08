e3f2437a2a97823c477ef737ed31351ecc48c7c5
Julian Meyn <Ic3Venom@gmail.com>
2017-10-07 23:31:09 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,47 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 int main()
 {
-    unsigned long long i, max, buffer;
-    short br = 0;
-    FILE *fPrime = fopen("primelistc.bin", "wb+");
+    unsigned long long int i, j, max, buffer, count;
+    short br, wr;
+    FILE *output, *fp;
 
-    printf("Finding the primes between 2 and what number? ");
-    scanf("%llu", &max);
 
+    fp = fopen("primes.bin", "wb+");
+
+    printf("Finding the primes between 2 and what number? \n");
+
+    max = 100000000;
+    wr = 0;
     buffer = 2;
-    fwrite(&buffer, sizeof(buffer), 1, fPrime);
-    exit(0);
+    count = 1;
+
+    fwrite((const void*)&buffer, sizeof(ULLONG_MAX), 1, fp);
+    fseek(fp, 0, SEEK_SET);
 
     for(i = 3; i <= max; i+= 2)
     {
-        fseek(fPrime, 0, SEEK_SET);
+        double guess = 1;
+        br = 0; //initialize or reset from previous loop
+
+        {
+            for(j = 0; j < 15; j++)
+            {
+                guess = (0.5) * (guess + ((double)i/guess));
+            }
+        }
+
+        fseek(fp, 0, SEEK_SET);
 
         while( 1 )
         {
-            fread((void*)(&buffer), sizeof(buffer), 1, fPrime);
-            printf("%llu", buffer);
-            if(feof(fPrime))
-            {
-                break;
-            }
+            fread(&buffer, sizeof(ULLONG_MAX), 1, fp);
+
             if(i % buffer == 0)
             {
                 br = 1;
                 break;
             }
+
+            if ((double)buffer > guess)
+            {
+                break;
+            }
+
+            if(feof(fp))
+            {
+                break;
+            }
         }
-        if(!br)
+
+        if(br == 0)
         {
-            printf("\n%llu", buffer);
-            fwrite(&buffer, sizeof(long), 1, fPrime);
+            fseek(fp, 0, SEEK_END);
+            fwrite((const void*)&i, sizeof(ULLONG_MAX), 1, fp);
+            count++;
         }
-
-        br = 0;
-
     }
 
-    fclose(fPrime);
-    return 0;
+    printf("total primes from 2 to %llu: %llu", max, count);
+
+    if( wr )
+    {
+        output = fopen("primes.txt", "w");
+        fseek(fp, 0, SEEK_SET);
+        while( count > 0 )
+        {
+            count--;
+            fread(&buffer, sizeof(ULLONG_MAX), 1, fp);
+            fprintf(output, "%llu\n", buffer);
+        }
+        fclose(output);
+    }
+
+    fclose(fp);
 }
